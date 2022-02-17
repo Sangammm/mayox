6088a63749c709d27e4413543b8eadbfb6d16190
Sangam Rajpara <sangamrajpara@gmail.com>
2022-02-17 22:59:56 +0530

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      black : "#121212",
+      grey: '#292929'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
