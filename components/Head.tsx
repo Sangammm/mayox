5157a6c9cb3304b3aaf30864c09d67655ce11366
Sangam Rajpara <sangamrajpara@gmail.com>
2022-02-24 01:29:20 +0530

    Title And Favicon

---
+++ components/Head.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const CommonHead = () => {
+  return (
+    <div>
+      <Head>
+        <title>Mayox</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+    </div>
+  )
+}
+
+export default CommonHead;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Card from '../components/Card'
 import Content from '../components/Content'
+import CommonHead from '../components/Head'
 import { Header } from '../components/Header'
 
 import Intro from '../components/ThumbCutter/Intro'
@@ -9,6 +10,7 @@ import IntroImage from '../components/ThumbCutter/IntroImage'
 const Home: NextPage = () => {
 	return (
 		<>
+			<CommonHead />
 			<Header />
 			<Content>
 				<Card left={<Intro />} right={<IntroImage />} />
