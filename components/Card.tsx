416e3d399d9d3a547d6927f0f7d83285df6c1123
Sangam Rajpara <sangamrajpara@gmail.com>
2022-02-24 00:55:16 +0530

    Mobile responsive Card

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,7 +7,7 @@ export type CardProps = {
 
 const Card: FunctionComponent<CardProps> = ({ left:Left, right:Right }) => {
 	return (
-		<div className="grid grid-cols-2 px-40 py-20 bg-grey min-h-fit">
+		<div className="grid grid-cols-1 px-1 py-20 bg-grey min-h-fit sm:grid-cols-2 sm:px-20 md:px-40">
       {Left}
 			{Right}
 		</div>
