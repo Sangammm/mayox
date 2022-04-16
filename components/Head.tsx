9359c01ed0dc67f3ef79e87aef768cf2ade56871
Sangam Rajpara <sangamrajpara@gmail.com>
2022-04-16 17:34:09 +0530

    Meta and tags

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,6 +6,8 @@ const CommonHead = () => {
       <Head>
         <title>Mayox</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Mayox creates simple yet innovative products that will make you life easier" />
+        <meta name="keywords" content="mayox, mayox plastic products, mayox plastic, mayox dying, thumb cutter, mayox thumb cutter, mayox contacts" />
       </Head>
     </div>
   )
