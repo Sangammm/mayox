793d24461f72d798f9584c4b9175d6e4b4a46026
Sangam Rajpara <sangamrajpara@gmail.com>
2023-02-04 19:02:18 +0530

    Update DoorKnboCard.tsx

--- components/DoorKnob/DoorKnboCard.tsx
+++ components/DoorKnob/DoorKnboCard.tsx
@@ -12,8 +12,7 @@ export const DoorKnboCard = () => {
 				<Intro
 					subTitle="Knob"
 					textArray={[
-						`Handles and knobs are often overlooked. The right knobs and pulls can make the simplest cabinet fronts look amazing. Knobs with distinctive designs draw the eye and are an important finishing touch for cabinetry. Choose high-quality knobs and pulls that will last longer and look good after years of use.`,
-						`ABVIN Solid Brass Round Knobs for Cabinet Kitchen Drawer Pulls knob ( 30 mm ) - Satin Brass - 1 Pcs`,
+						`Handles and knobs are often overlooked. The right knobs and pulls can make the simplest cabinet fronts look amazing. Knobs with distinctive designs draw the eye and are an important finishing touch for cabinetry. Choose high-quality knobs and pulls that will last longer and look good after years of use.`
 					]}
 				>
 					<Button onClick={() => downloadPdf('cabinet_knob.pdf', 'Mayox Cabinate Knob')}>
